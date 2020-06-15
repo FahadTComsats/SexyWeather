ed60c926da8a74fc38e19ddd8243a1a49d1d6933
Fahad Tariq <47610084+FahadTComsats@users.noreply.github.com>
2020-06-15 17:51:02 +0500

    Initial stages

---
+++ SexyWeather/Controllers/WeatherViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SexyWeather
+//
+//  Created by Fahad Tariq on 15/06/2020.
+//  Copyright © 2020 Fahad Tariq. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionWeatherImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
